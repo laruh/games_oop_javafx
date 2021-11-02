c84bb2873d626fea37f8317840710e8b9dbc13e9
laruh <oblomoff616@gmail.com>
2021-11-02 18:27:02 +0700

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -22,11 +22,9 @@ public final class Logic {
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
-            if (figure != null) {
-                for (Cell step : steps) {
-                    if (figure != null && figure.position().equals(step)) {
-                        throw new OccupiedCellException("There are other figures on steps");
-                    }
+            for (Cell step : steps) {
+                if (figure != null && figure.position().equals(step)) {
+                    throw new OccupiedCellException("There are other figures on steps");
                 }
             }
         }
